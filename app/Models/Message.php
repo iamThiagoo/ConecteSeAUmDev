917a3ba6bab5b50ea61040d193bb55d0be2a9820
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-05-16 23:25:33 -0300

    Relationships

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,19 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Action extends Model
+class Message extends Model
 {
     use HasFactory;
 
-    //
+    // 
     public function fromUser () : BelongsTo
     {
         return $this->belongsTo(User::class, 'from_user_id');
     }
 
-    // 
+    //
     public function toUser () : BelongsTo
     {
-        return $this->belongsTo(User::class, 'to_user_id');
+        return $this->belongTo(User::class, 'to_user_id');
     }
 }
