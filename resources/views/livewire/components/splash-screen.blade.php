d9063b59682d005cef352814cbf0db314c653557
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-05-19 23:52:13 -0300

    Update in animation

--- resources/views/livewire/components/splash-screen.blade.php
+++ resources/views/livewire/components/splash-screen.blade.php
@@ -3,13 +3,13 @@
     x-init="initSplash"
 >
     <div x-ref="slidecontainer" 
-        class="flex flex-col justify-center items-center min-h-screen w-full bg-gradient-to-r from-[#2000ab] to-[#00b2d5]"
+        class="absolute top-0 left-0 transform duration-1000 flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-r from-[#2000ab] to-[#00b2d5]"
     >
-        <div class="flex flex-col items-center" data-aos="fade-up" data-aos-offset="0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-20 h-16">
+        <div class="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-16 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
             </svg>              
-            <h2 class="text-white text-xl mt-2">
+            <h2 class="mt-2 text-xl text-white">
                 Conecte-se com Um Dev
             </h2>
         </div>
@@ -23,12 +23,22 @@
         return {
             initSplash ()
             {
-
-            }
-
-            animate (element, classList, type) 
+                this.animate (this.$refs.slidecontainer, ['left-full', 'skew-x-12'], 'add', 1000, 
+                    () => this.animate(this.$refs.slidecontainer, ['skew-x-12'], 'remove', 400)
+                )
+            },
+            animate (element, classList, type, time, callback) 
             {
+                setTimeout(() => {
+                    if (type === 'add') 
+                        element.classList.add(...classList)
+                    else
+                        element.classList.remove(...classList)
 
+                    if (callback)
+                        calback();
+
+                }, time ? time : 1000);
             }
         }
     }
