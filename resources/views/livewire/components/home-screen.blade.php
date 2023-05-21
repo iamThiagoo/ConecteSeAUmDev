877b932ec8d001f07ac274a4062f4a884d6a7375
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-05-21 10:00:21 -0300

    HomeScreen layout completed

--- resources/views/livewire/components/home-screen.blade.php
+++ resources/views/livewire/components/home-screen.blade.php
@@ -3,7 +3,7 @@
     <div
         class="absolute top-0 left-0 transform duration-1000 flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-r from-[#2000ab] to-[#00b2d5]"
     >
-        <div class="flex flex-1 flex-col justify-end">
+        <div class="flex flex-1 flex-col justify-end items-center mt-24">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-16 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
             </svg>
@@ -12,15 +12,20 @@
             </h2>
         </div>
 
-        <div class="flex flex-1 flex-col justify-end">
+        <div class="flex flex-1 flex-col justify-center px-8">
             <p class="text-xs text-white text-center">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur placeat maxime rem quis accusantium eum minus.
                 Dolores iure, vel molestiae, ullam, odio beatae sint rem iusto odit velit esse adipisci!
             </p>
             <div>
+                <button class="border-2 border-white bg-white w-full p-4 mt-6 text-gray-100 text-sm rounded-full transform duration-150 active:scale-95">
+                    Criar uma conta
+                </button>
 
+                <button class="border-solid border-2 border-white w-full p-4 mt-4 mb-8 text-white text-sm rounded-full transform duration-150 active:scale-95">
+                    Entrar
+                </button>
             </div>
         </div>
-
     </div>
 </div>
