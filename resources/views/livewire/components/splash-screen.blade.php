7ae075431b220bf8d34c6e4d7e9b9d2fb5024d77
Thiago Ferreira <devthiagoferreira@gmail.com>
2023-05-20 21:13:06 -0300

    Update on SplashScreen

--- resources/views/livewire/components/splash-screen.blade.php
+++ resources/views/livewire/components/splash-screen.blade.php
@@ -8,13 +8,21 @@
         <div class="flex flex-col items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-16 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
-            </svg>              
+            </svg>
             <h2 class="mt-2 text-xl text-white">
                 Conecte-se com Um Dev
             </h2>
         </div>
     </div>
 
+    <div class="flex flex-col items-center justify-center min-h-screen w-full">
+        <svg x-ref="logo" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-16 text-blue-500 scale-0 duration-1000">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
+        </svg>
+        <h2 x-ref="textLogo" class="mt-2 text-xl text-blue-500 transform scale-0 duration-1000">
+            Conecte-se com Um Dev
+        </h2>
+    </div>
 </div>
 
 <script>
@@ -23,21 +31,28 @@
         return {
             initSplash ()
             {
-                this.animate (this.$refs.slidecontainer, ['left-full', 'skew-x-12'], 'add', 1000, 
-                    () => this.animate(this.$refs.slidecontainer, ['skew-x-12'], 'remove', 400)
+                document.body.classList.add('overflow-hidden');
+
+                this.animate(this.$refs.slidecontainer, ['left-full', 'skew-x-12'], 'add', 1000,
+                    () => this.animate(this.$refs.slidecontainer, ['skew-x-12'], 'remove', 400,
+                        () => this.animate(this.$refs.logo, ['scale-100'], 'add', 300,
+                            () => this.animate(this.$refs.textLogo, ['scale-100'], 'add', 300)
+                        )
+                    )
                 )
             },
             animate (element, classList, type, time, callback) 
             {
                 setTimeout(() => {
-                    if (type === 'add') 
-                        element.classList.add(...classList)
-                    else
-                        element.classList.remove(...classList)
+                    if (type === 'add') {
+                        element.classList.add(...classList);
+                    } else {
+                        element.classList.remove(...classList);
+                    }
 
-                    if (callback)
-                        calback();
-
+                    if (callback) {
+                        callback();
+                    }
                 }, time ? time : 1000);
             }
         }
